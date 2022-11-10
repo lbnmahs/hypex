19b5a35286728584b4efdfce479d9e8f06453b5f
LbnMahs <labanmahs2002@gmail.com>
2022-11-10 08:48:50 +0300

    App Icon inclusion

--- app/src/main/java/com/example/hypex/activities/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/hypex/activities/LoginRegisterActivity.kt
@@ -2,9 +2,11 @@ package com.example.hypex.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.hypex.R
 import dagger.hilt.android.AndroidEntryPoint
 
+@Suppress("DEPRECATION")
 @AndroidEntryPoint
 class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

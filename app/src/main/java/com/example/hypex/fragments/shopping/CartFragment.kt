c6f3c2fb9e4c085ccbf3b7d1a80511f4c43ece89
LbnMahs <labanmahs2002@gmail.com>
2022-11-29 20:43:49 +0300

    Design changes

--- app/src/main/java/com/example/hypex/fragments/shopping/CartFragment.kt
+++ app/src/main/java/com/example/hypex/fragments/shopping/CartFragment.kt
@@ -1,8 +1,22 @@
 package com.example.hypex.fragments.shopping
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.hypex.R
+import com.example.hypex.databinding.FragmentCartBinding
 
 class CartFragment: Fragment(R.layout.fragment_cart) {
+    private lateinit var binding: FragmentCartBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCartBinding.inflate(inflater)
+        return binding.root
+    }
 
 }
--- app/src/main/java/com/example/hypex/fragments/shopping/ProfileFragment.kt
+++ app/src/main/java/com/example/hypex/fragments/shopping/ProfileFragment.kt
@@ -1,9 +1,22 @@
 package com.example.hypex.fragments.shopping
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.hypex.R
+import com.example.hypex.databinding.FragmentProfileBinding
 
 class ProfileFragment: Fragment(R.layout.fragment_profile) {
-
+    private lateinit var binding: FragmentProfileBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater)
+        return binding.root
+    }
 
 }
